3c06bcf38370f059d3e27f6de5de8230fd957601
Karunya Vijayarajan <karunsap26@gmail.com>
2022-04-20 18:29:47 -0500

    Data binding

--- conFusion/src/app/shared/dish.ts
+++ conFusion/src/app/shared/dish.ts
@@ -1,18 +1,15 @@
-export class Dish{
-    id?: string;
-    name?: string;
-    image?: string;
-    category?: string;
-    featured?: boolean;
-    label?: string;
-    price?: string;
-    description?: string;
-    comments?:IComments[];
+import { Comment } from "./comment";
+
+export interface Dish{
+    id: string;
+    name: string;
+    image: string;
+    category: string;
+    featured: boolean;
+    label: string;
+    price: string;
+    description: string;
+    comments:Comment[];
+   
 }
 
-export class IComments{
-        rating?:number;
-        comment?:string;
-        author?:string;
-        date?:string; 
-}
