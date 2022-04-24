ac2f24dea120813920241b65eaa13584156842af
Karunya Vijayarajan <karunsap26@gmail.com>
2022-04-24 18:00:12 -0500

    SPA part2

--- conFusion/src/app/dishdetail/dishdetail.component.ts
+++ conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { DishService } from '../services/dish.service';
 import { Dish } from '../shared/dish';
 
 @Component({
@@ -7,11 +10,18 @@ import { Dish } from '../shared/dish';
   styleUrls: ['./dishdetail.component.scss']
 })
 export class DishdetailComponent implements OnInit {
-  @Input()
+ 
   dish?:Dish;
-  constructor() { }
+  constructor(private dishservice: DishService,
+    private route: ActivatedRoute,
+    private location: Location) { }
 
   ngOnInit(): void {
+    
+    const id = this.route.snapshot.params['id'];
+    this.dish = this.dishservice.getDish(id);
   }
-
+  goBack(): void {
+    this.location.back();
+  }
 }
