886424af3ec73a4450389b5743bb59cceb9d39ea
Karunya Vijayarajan <karunsap26@gmail.com>
2022-06-01 10:23:56 -0500

    Https

--- app.js
+++ app.js
@@ -20,6 +20,16 @@ var leaderRouter = require('./routes/leaderRouter');
 
 var app = express();
 
+// Secure traffic only
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  }
+  else {
+    res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
+  }
+});
+
 const url= config.mongoUrl;                            
 const connect = mongoose.connect(url);
 
